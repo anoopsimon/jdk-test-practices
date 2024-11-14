46ae2462804e3232dae2209364d87d499310c932
Anoop Simon <Anoop.Simon@health.vic.gov.au>
2024-11-15 10:38:10 +1100

    added jdk 11 compatibility

--- src/main/java/org/example/A01.java
+++ src/main/java/org/example/A01.java
@@ -1,15 +1,5 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class A01
-{
-    public static void main( String[] args )
-    {
-
-        System.out.println( "Hello World!" );
-
-    }
+public class A01 {
+    //very difficult. not getting answer.checking
 }
